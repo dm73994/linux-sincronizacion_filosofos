a159311ea2e3045bdf6a2adc26b272dd20bda46c
Carlos David Mesa Martinez <dm31284@gmail.com>
2022-11-22 19:57:25 -0500

    all

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['linux_2dsincronizacion_5ffilosofos_0',['linux-sincronizacion_filosofos',['../d0/d30/md_README.html',1,'']]]
+];
---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "l",
+  1: "l"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Todo",
+  1: "Páginas"
+};
+
